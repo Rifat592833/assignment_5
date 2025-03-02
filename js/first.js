13a814a6c3b9221289c9b0d2d060c4416362a286
Rifat592833 <mahmudulhasanrifat79@gmail.com>
2025-03-02 03:12:05 -0800

    After complete card section

--- js/first.js
+++ js/first.js
@@ -7,3 +7,21 @@ function changeBackgroundColor() {
     // Apply the random color to the background
     document.body.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
 }
+
+let totalCount = 23; // Single counter for all buttons
+let decreasingCount = 6;
+
+function increaseCounter(button) {
+    if (!button.classList.contains("clicked")) {  
+        totalCount++; 
+        document.getElementById("totalCounter").innerText = totalCount; 
+        button.classList.add("clicked"); 
+        button.style.backgroundColor = "rgba(55, 82, 253, 0.2)"; // 20% opacity
+        button.classList.add("cursor-not-allowed"); 
+        button.disabled = true;  
+        if (decreasingCount > 0) {
+            decreasingCount--;
+            document.getElementById("decreasingCounter").innerText = decreasingCount;
+        }
+    }
+}
